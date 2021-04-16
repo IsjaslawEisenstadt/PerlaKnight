1f996580ff71b59878d2319b910b8ea60ae12f7c
DarkMode <I.Eisenstadt@gmx.de>
2021-04-16 12:31:21 +0200

    add an LDTK importer plugin
    I made two slight changes to it:
     - fixed collision int grid layers
     - added QoL support for reloading inheriting scenes when importing

---
+++ addons/LDtk-Importer/LDtkImporterPlugin.gd
@@ -0,0 +1,25 @@
+tool
+extends EditorPlugin
+
+var import_plugin = null
+
+func get_plugin_name():
+	return "LDtk Importer"
+
+func _enter_tree():
+	import_plugin = preload("LDtkImporter.gd").new()
+	import_plugin.connect("import_finished", self, "on_import_finished")
+	add_import_plugin(import_plugin)
+
+func _exit_tree():
+	remove_import_plugin(import_plugin)
+	import_plugin.disconnect("import_finished", self, "on_import_finished")
+	import_plugin = null
+
+
+func on_import_finished(path) -> void:
+	var scene_path = path.get_basename() + ".tscn"
+	var eif := get_editor_interface()
+	for scene in eif.get_open_scenes():
+		if scene == scene_path:
+			eif.reload_scene_from_path(scene)
