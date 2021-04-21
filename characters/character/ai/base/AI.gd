b3befcc1f115d5ee8fbaafe7a145de377d6974da
DarkMode <I.Eisenstadt@gmx.de>
2021-04-21 15:45:44 +0200

    add base ai state, state machine and input controller

---
+++ characters/character/ai/base/AI.gd
@@ -0,0 +1,12 @@
+extends State
+class_name AI
+
+"""
+base state for the AIStateMachine
+every AI script has access to the host and input_controller
+"""
+
+#warning-ignore:unused_class_variable
+var host: Character
+#warning-ignore:unused_class_variable
+var input_controller: AIInputController
