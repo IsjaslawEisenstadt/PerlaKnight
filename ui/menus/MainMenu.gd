01bfdc446e8e6f42fa17e8bce21cdf37dede343e
DarkMode <I.Eisenstadt@gmx.de>
2021-04-26 19:41:51 +0200

    remove exit button from HTML5

--- ui/menus/MainMenu.gd
+++ ui/menus/MainMenu.gd
@@ -7,6 +7,10 @@ onready var Transition := get_node(transition_path) as Transition
 export var play_state_path: NodePath = "../PlayState"
 export var transition_path: NodePath = "../Transition"
 
+func _ready() -> void:
+	if OS.has_feature("HTML5"):
+		$Margin/VBox/ButtonCenter/ButtonVBox/ExitButton.visible = false
+
 func _on_NewGameButton_pressed() -> void:
 	Transition.start("fade_out")
 	yield(Transition, "transition_finished")
