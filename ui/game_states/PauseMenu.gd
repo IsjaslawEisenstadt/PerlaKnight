db65400c87e006f87936ac73065f41553185e6b4
tlohm12s <63550138+tlohm12s@users.noreply.github.com>
2021-05-05 15:28:02 +0200

    Basic Pause Logic, Fixed Transition

--- ui/game_states/PauseMenu.gd
+++ ui/game_states/PauseMenu.gd
@@ -1,6 +1,7 @@
 extends GameState
 class_name PauseMenu
 
+onready var MainMenu := $".."/MainMenu
 export(String, FILE, "*.tscn,*.scn,*.ldtk") var new_game_scene_path: String
 
 func _ready() -> void:
@@ -8,8 +9,11 @@ func _ready() -> void:
 		$Margin/VBox/ButtonCenter/ButtonVBox/ExitButton.visible = false
 
 func _on_BackToMenuButton_pressed():
+	#Maybe save-logic here
+	if state_machine._pop_push(MainMenu):
+		return
 	pass
-
 
 func _on_ContinueButton_pressed():
-	pass
+	if state_machine._pop_state():
+		return
--- ui/game_states/PlayState.gd
+++ ui/game_states/PlayState.gd
@@ -1,6 +1,7 @@
 extends GameState
 class_name PlayState
 
+onready var PauseMenu = $".."/UI/PauseMenu
 onready var Transition := $".."/UI/Transition
 onready var PlayUI := $".."/UI/PlayUI
 
@@ -8,21 +9,23 @@ var current_scene: Node
 
 func _state_enter(previous_state: State, params: Dictionary = {}) -> void:
 
-	assert("scene" in params && params.scene is Node)
-	
-	if current_scene:
-		remove_child(current_scene)
-		current_scene.queue_free()
-	
-	current_scene = params.scene
-	add_child(params.scene)
-	current_scene.set_ui(PlayUI)
+	if !get_tree().paused:
+		assert("scene" in params && params.scene is Node)
+		
+		if current_scene:
+			remove_child(current_scene)
+			current_scene.queue_free()
+		
+		current_scene = params.scene
+		add_child(params.scene)
+		current_scene.set_ui(PlayUI)
 	
 	._state_enter(previous_state, params)
-	
+
 	Transition.start("fade_in")
 	yield(Transition, "transition_finished")
 
 func _state_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
-		pass
+		if state_machine._push_state(PauseMenu):
+			return
