b4c76b58af1b768e0c30520840108a68326b127f
tlohm12s <63550138+tlohm12s@users.noreply.github.com>
2021-05-18 16:53:18 +0200

    Activated blur and scaling fix

--- ui/interface/rune_ability_interface/RuneAbilityEntryContainer.gd
+++ ui/interface/rune_ability_interface/RuneAbilityEntryContainer.gd
@@ -1,7 +1,8 @@
 extends MarginContainer
  
-onready var RuneTextureRect := $HBoxContainer/RuneTextureRect
-onready var DescriptionRichTextLabel := $HBoxContainer/DescriptionRichTextLabel as RichTextLabel
+
+onready var RuneTextureRect := $HBoxContainer/RuneTextureRect as TextureRect
+onready var DescriptionRichTextLabel := $HBoxContainer/MarginContainer/DescriptionRichTextLabel as RichTextLabel
 
 func init(RuneResource : Resource):
 	DescriptionRichTextLabel.text = RuneResource.description
