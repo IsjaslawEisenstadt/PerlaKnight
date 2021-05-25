6522923c119cead8dddec52da194f56322ae2b59
tlohm12s <63550138+tlohm12s@users.noreply.github.com>
2021-05-25 16:59:15 +0200

    Option for ai to follow player

--- characters/character/ai/base/SearchAI.gd
+++ characters/character/ai/base/SearchAI.gd
@@ -5,6 +5,7 @@ onready var FollowAI := get_node_or_null(follow_state_path) as AI
 
 export var follow_state_path: NodePath = "../FollowAI"
 export var search_distance: int = 70
+export var can_follow = false
 
 var distance_to_player: int = 0
 	
@@ -12,6 +13,6 @@ func _ready() -> void:
 	Perception.connect("body_entered", self, "on_body_perceived")
 
 func on_body_perceived(body) -> void:
-	if body is Player && state_machine:
+	if body is Player && state_machine && can_follow:
 		if state_machine._pop_push(FollowAI, { 'player': body }):
 			return
