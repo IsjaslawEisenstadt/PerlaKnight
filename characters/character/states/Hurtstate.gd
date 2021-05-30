b9bb7ae5a3fc02b3c9a3c209b017c5296635e893
Isjaslaw Eisenstadt <I.Eisenstadt@gmx.de>
2021-05-30 19:45:04 +0200

    improved hurt state

--- characters/character/states/Hurtstate.gd
+++ characters/character/states/Hurtstate.gd
@@ -1,7 +1,11 @@
 extends PhysicsState
 class_name HurtState
 
-export var kickback_strength: float = 1000.0
+onready var IdleState := get_node(idle_state_path)
+
+export var idle_state_path: NodePath = "../IdleState"
+
+export var kickback_strength: float = 550.0
 
 func _state_enter(previous_state: State, params: Dictionary = {}) -> void:
 	._state_enter(previous_state, params)
@@ -9,21 +13,28 @@ func _state_enter(previous_state: State, params: Dictionary = {}) -> void:
 	assert("attacker" in params)
 	var attacker: Node2D = params.attacker
 	
-	host.velocity = (host.global_position - attacker.global_position).normalized() * kickback_strength
 	host.invincible = true
 	var direction := int(sign(attacker.global_position.x - host.global_position.x))
 	if direction != 0:
 		host.look_direction = direction
+	
+	host.velocity.x = kickback_strength * -direction
+	host.velocity.y = ((host.global_position - attacker.global_position).normalized() * kickback_strength).y
 
 func _on_animation_finished(finished_animation_name: String) -> void:
 	._on_animation_finished(finished_animation_name)
-	state_machine._pop_state()
+	state_machine._push_state(IdleState)
 
 func _state_physics_process(delta: float) -> void:
 	._state_physics_process(delta)
+	
 	# TODO: unify this with the current PhysicsState.move() function
 	host.velocity *= pow(1.0 - current_move_damping, delta * 10.0)
 	fall(delta)
+	
+	host.velocity.x = clamp(host.velocity.x, -kickback_strength, kickback_strength)
+	host.velocity.y = clamp(host.velocity.y, -kickback_strength, kickback_strength)
+	
 	host.velocity = host.move_and_slide(host.velocity, FLOOR_DIRECTION)
 
 func _state_exit(next_state: State) -> void:
