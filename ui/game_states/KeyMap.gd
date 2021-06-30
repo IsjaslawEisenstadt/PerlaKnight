9fcd371c5389680ec51235fa4aecb91fceb74563
tlohm12s <63550138+tlohm12s@users.noreply.github.com>
2021-06-30 20:23:02 +0200

    Custom KeyMap

--- ui/game_states/KeyMap.gd
+++ ui/game_states/KeyMap.gd
@@ -1,20 +1,9 @@
 extends GameState
 class_name KeyMap
 
-export var KeyMapListEntry: PackedScene
-
 func _state_enter(previous_state: State, params: Dictionary = {}) -> void:
-	assert(KeyMapListEntry)
-	
 	pause_game = true
 	._state_enter(previous_state, params)
-	
-	for action in InputMap.get_actions():
-		if InputMap.get_action_list(action).size() > 0:
-			var new_entry = KeyMapListEntry.instance()
-			$Content/VBoxContainer/ScrollContainer/VBoxContainer.add_child(new_entry)
-			
-			new_entry.add_action(action)
 
 func _state_exit(_next_state: State, _params: Dictionary = {}) -> void:
 	._state_exit(_next_state, _params)
--- ui/general/keymap/KeyMapListEntry.gd
+++ ui/general/keymap/KeyMapListEntry.gd
@@ -1,16 +1,19 @@
 extends Container
 class_name KeyMapListEntry
 
-export var InputEntry : PackedScene
+export var InputEntry: PackedScene
+
+export (String) var action_name
+export (Array, Texture) var input_entry_textures
 
 func _ready() -> void:
 	assert(InputEntry)
-
-func add_action(action) -> void:
-	$MarginContainer/HBoxContainer/Action.text = action
+	assert(input_entry_textures)
+	assert(action_name)
 	
-	for input in InputMap.get_action_list(action):
-		var new_entry : RichTextLabel = InputEntry.instance() as RichTextLabel
-		new_entry.text = input.as_text()
-		
-		$MarginContainer/HBoxContainer/InputEntryList.add_child(new_entry)
+	$MarginContainer/HBoxContainer/Action.text = action_name
+	
+	for texture in input_entry_textures:
+		var new_entry = InputEntry.instance()
+		new_entry.texture = texture
+		$MarginContainer/HBoxContainer/InputEntryList.add_child(new_entry)		
