ced7f337ef6b31c4509a943008f05f9608da022e
tlohm12s <63550138+tlohm12s@users.noreply.github.com>
2021-07-01 16:38:17 +0200

    New Layout

--- ui/general/keymap/KeyMapListEntry.gd
+++ ui/general/keymap/KeyMapListEntry.gd
@@ -1,19 +1,29 @@
+tool
 extends Container
 class_name KeyMapListEntry
 
 export var InputEntry: PackedScene
 
-export (String) var action_name
-export (Array, Texture) var input_entry_textures
+export (String) var action_name setget set_action_name
+export (Array, Texture) var input_entry_textures setget set_input_entry_textures
 
 func _ready() -> void:
 	assert(InputEntry)
 	assert(input_entry_textures)
 	assert(action_name)
-	
+
+func set_action_name(new_action_name):
+	action_name = new_action_name
 	$MarginContainer/HBoxContainer/CenterContainer/Action.text = action_name
 	
+func set_input_entry_textures(new_input_entry_textures):
+	input_entry_textures = new_input_entry_textures
+	
+	for child in $MarginContainer/HBoxContainer/MarginContainer/InputEntryList.get_children():
+		$MarginContainer/HBoxContainer/MarginContainer/InputEntryList.remove_child(child)
+		
 	for texture in input_entry_textures:
 		var new_entry = InputEntry.instance()
 		new_entry.get_node("Texture").texture = texture
 		$MarginContainer/HBoxContainer/MarginContainer/InputEntryList.add_child(new_entry)		
+	
