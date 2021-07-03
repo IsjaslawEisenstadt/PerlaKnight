14c5e792dafce31d999a939f73cf3c7187bc3c6b
Isjaslaw Eisenstadt <I.Eisenstadt@gmx.de>
2021-07-03 11:06:28 +0200

    prepare for merge

--- ui/hud/help_hud/HelpHUD.gd
+++
@@ -1,8 +0,0 @@
-extends MarginContainer
-class_name HelpHUD
-
-func on_player_connected(player: Player) -> void:
-	visible = true
-
-func on_player_disconnected(player: Player) -> void:
-	visible = false
