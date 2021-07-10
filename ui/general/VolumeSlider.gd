e112b5527ddaac9b5181e7240d1b803faa271c3c
Isjaslaw Eisenstadt <I.Eisenstadt@gmx.de>
2021-07-10 10:48:28 +0200

    fix pause menu audio sliders getting out of sync with main menu sliders

--- ui/general/VolumeSlider.gd
+++ ui/general/VolumeSlider.gd
@@ -7,7 +7,11 @@ export var audio_bus_name := "Master"
 
 func _ready() -> void:
 	value = Config.get_volume(audio_bus_name, db2linear(AudioServer.get_bus_volume_db(Bus)))
+	connect("visibility_changed", self, "on_visibility_changed")
 
 func on_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(Bus, linear2db(value))
 	Config.set_volume(audio_bus_name, value)
+
+func on_visibility_changed() -> void:
+	value = Config.get_volume(audio_bus_name, db2linear(AudioServer.get_bus_volume_db(Bus)))
